20de51cf2fc4909561fb5598579c2e8c499b5cbd
asif <asif69284@gmail.com>
2024-07-15 13:14:24 +0545

    name and email address placeholder updated

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -79,7 +79,7 @@ export default function Signup() {
                             <div>
                                 <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
                                 <div className="mt-2">
-                                    <input id="name" name="name" type="name" required className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-offset-secondary sm:text-sm sm:leading-6" />
+                                    <input id="name" name="name" placeholder='Your Name' type="name" required className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-offset-secondary sm:text-sm sm:leading-6" />
                                 </div>
                             </div>
 
